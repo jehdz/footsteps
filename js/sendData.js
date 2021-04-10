4f119ebd00097feb271dd4fbda033b4d66885ccd
jehdz <jhernandez23@luc.edu>
2021-04-10 15:21:54 -0500

    Location button fucntionality

---
+++ js/sendData.js
@@ -0,0 +1,30 @@
+const submit = document.getElementById('submit')
+
+
+submit.addEventListener('click', () => {
+
+    const name = document.getElementById('name').value;
+    const description = document.getElementById('description').value;
+    const category = document.getElementById('category').value;
+    const address = document.getElementById('address').value;
+
+
+    if (name.length == 0 || description.length == 0 || category.length == 0 || address.length == 0) {
+        alert('Please fill out form');
+    } else {
+
+        var newEntry = {
+            "name": name,
+            "description": description,
+            "category": category,
+            "address": address
+        }
+        locationData.push(newEntry);
+        alert('Thank you for submitting! our Team will review your entry');
+
+    }
+
+
+    //For now, this line is just for testing purposes
+    console.log(locationData.length);
+});
