0954f0f9f9fe5681d8c0ad7d938bb53628c0810c
oyane_5ab4y9h <oyanek@gmail.com>
2021-04-21 13:51:51 -0500

    marker and marker listener

--- app/assets/js/map.js
+++ app/assets/js/map.js
@@ -34,6 +34,21 @@ function initMap() {
         map.panTo(new google.maps.LatLng(nextLat, nextLong));
     });
 
+    var marker, i;
+
+    for(i=0; i< locations.length; i++) {
+        marker = new google.maps.Marker({
+            position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+            map: map
+        });
+
+        google.maps.event.addListener(marker, 'click', (function(marker, i) {return function() {
+            infowindow.setContent(locations[i][0]);
+            infowindow.open(map,marker);
+        }
+        })(marker, i));
+    }
+
     //}
 
 
