451aa7a9b60f1570a6846944e6d487eba57072fa
jehdz <jhernandez23@luc.edu>
2021-05-05 22:10:43 -0500

    Adding port for heroku

--- server.js
+++ server.js
@@ -39,8 +39,8 @@ app.post('/', (req, res) => {
 })
 
 
-app.listen(3000, ()=> {
-    console.log("Server is running on port 3000;")
+app.listen(process.env.PORT || 3000, ()=> {
+    console.log(`Server is running on port ${process.env.PORT}`)
 })
 
 
